6be928272ee96140865b9e742f6f491d5850c324
Sebastián Tinajeros <68345241+DraccoSTE@users.noreply.github.com>
2021-06-03 16:27:56 -0500

    Update Euclides_Cl.cpp

--- Euclides/ClassicMCD/Euclides_Cl.cpp
+++ Euclides/ClassicMCD/Euclides_Cl.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace NTL;
 
-ZZ modulo (ZZ a,ZZ n, char val)
+/*ZZ modulo (ZZ a,ZZ n, char val)
 {
 	//a = q*n + r  //0 =< r < n
 	ZZ q, r;
@@ -31,19 +31,32 @@ ZZ modulo (ZZ a,ZZ n, char val)
         return r;
     }
 
+}*/
+
+ZZ mod(ZZ a, ZZ b)
+{
+    ZZ r;
+    r = a - b * (a / b);
+    r = r + (r < 0) * b;
+    return r;
 }
+
 ZZ euclides (ZZ x, ZZ y, bool pasos)
 {
 	ZZ q,r;
-	q = modulo(x,y,'q');
-	r = modulo(x,y,'r');
+	//q = modulo(x,y,'q');
+	//r = modulo(x,y,'r');
+	q = mod(x,y);
+	r = mod(x,y);
 
 	while (r!=0)
 	{
 		x = y;
 		y = r;
-		q = modulo(x,y,'q');
-		r = modulo(x,y,'r');
+		//q = modulo(x,y,'q');
+		//r = modulo(x,y,'r');
+		q = mod(x,y);
+		r = mod(x,y);
 		x = q*y + r;
 		if (pasos == true)
         {
