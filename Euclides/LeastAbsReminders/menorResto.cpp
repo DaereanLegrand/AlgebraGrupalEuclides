4f439ce1bf5286b7c91b2d1e8b17232a2d9c1636
Sebastián Tinajeros <68345241+DraccoSTE@users.noreply.github.com>
2021-06-03 19:57:05 -0500

    Update menorResto.cpp

--- Euclides/LeastAbsReminders/menorResto.cpp
+++ Euclides/LeastAbsReminders/menorResto.cpp
@@ -20,12 +20,12 @@ void Menor_resto(RR a, RR b){
 		bZZ=abs(rZZ);
 		conv(r,rZZ); conv(a,aZZ); conv(b,bZZ);
 	}
-	cout<<"\nel mcd de menor resto es: "<<a<<endl;
-	}
+}
 
 int main (){
-	RR a; a=542312;
-	RR b; b=442321;
+	RR a;RR b;
+	a = conv<RR>("113961491159425124522547710339601516414581636984912436254300859090699424230641");
+	b = conv<RR>("83825486427085367934112210589772991484453445136409752382569536656995452034661");
 	Menor_resto(a,b);
 }
 
